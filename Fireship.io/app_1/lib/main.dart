a72758012609d889ce5f1331c26328e14fae7d14
Pranav Verma <pranav@verma.net.in>
2023-04-24 20:45:44 +0530

    Update main.dart

--- Fireship.io/app_1/lib/main.dart
+++ Fireship.io/app_1/lib/main.dart
@@ -11,23 +11,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App (Made by Pranav Verma - With Fireship.io)'),
-        ),
+          appBar: AppBar(
+            title:
+                Text('My First App (Made by Pranav Verma - With Fireship.io)'),
+          ),
+          body: Stack(
+            children: [
 
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Expanded(
-              flex: 4,
-              child: Icon(Icons.backpack),
-            ),
-            Icon(Icons.leaderboard),
-            Icon(Icons.person),
-          ],
-        )
-      ),
+            ],
+          )),
     );
-   }
+  }
 }
